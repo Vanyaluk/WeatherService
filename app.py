31570d286e8c0212fca6c6a38a6c46ffdf74e244
vanyaluk <ivanicloud_vanya@icloud.com>
2024-12-05 20:37:40 +0300

    Мини-сервер на Flask
    Создание и тестировка сервиса из двух страниц на фласке.

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        field1 = request.form.get("field1")
+        field2 = request.form.get("field2")
+
+        # запрос за данными
+
+
+        return redirect(url_for("result", field1=field1, field2=field2))
+    return render_template("index.html")
+
+
+@app.route("/result")
+def result():
+    field1 = request.args.get("field1", "")
+    field2 = request.args.get("field2", "")
+
+    return render_template("result.html", field1=field1, field2=field2)
 
 
 if __name__ == "__main__":
